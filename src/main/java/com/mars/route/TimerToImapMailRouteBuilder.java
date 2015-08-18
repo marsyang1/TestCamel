fcf4c4508d9df79301c430c100e26cb2d7b7f6ff
Mars Yang <marsyang1@gmail.com>
2015-08-18 17:07:24 +0800

    add test code

--- src/main/java/com/mars/route/TimerToImapMailRouteBuilder.java
+++ src/main/java/com/mars/route/TimerToImapMailRouteBuilder.java
@@ -1,6 +1,7 @@
 package com.mars.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.routepolicy.quartz.CronScheduledRoutePolicy;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,8 +15,11 @@ public class TimerToImapMailRouteBuilder extends RouteBuilder {
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
-        from("timer://timer1?period=10s&daemon=false")
-                .to("imaps://localhost?username=mars@mars.bb.com&password=123456&debugMode=true&delete=false&unseen=true&consumer.delay=60000")
+        CronScheduledRoutePolicy startPolicy = new CronScheduledRoutePolicy();
+        startPolicy.setRouteStartTime("*/3 * * * * ?");
+
+        from("imap://localhost?username=mars@mars.bb.com&password=123456&debugMode=true&delete=false&unseen=true&consumer.delay=60000")
+                .routePolicy(startPolicy)
                 .convertBodyTo(String.class)
                 .log("mail >> ${body}");
     }
