07400894a38bf542431f1c00bd3a955aef685115
Mars Yang <marsyang1@gmail.com>
2015-08-24 15:41:59 +0800

    add  process

---
+++ src/main/java/com/mars/process/FilterHtmlTagProcess.java
@@ -0,0 +1,23 @@
+package com.mars.process;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by mars on 2015/8/24.
+ */
+@Slf4j
+@Component
+public class FilterHtmlTagProcess implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Message in = exchange.getIn();
+        String s = in.getBody().toString();
+        log.info("body = " + s);
+        in.setBody(in.getBody(String.class).replaceAll("<[^>]*>", ""));
+    }
+}
--- src/main/java/com/mars/route/TimerToPop3MailRouteBuilder.java
+++ src/main/java/com/mars/route/TimerToPop3MailRouteBuilder.java
@@ -1,13 +1,19 @@
 package com.mars.route;
 
+import com.mars.process.FilterHtmlTagProcess;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.routepolicy.quartz.CronScheduledRoutePolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * A Camel Java DSL Router
  */
-public class TimerToImapMailRouteBuilder extends RouteBuilder {
+@Component
+public class TimerToPop3MailRouteBuilder extends RouteBuilder {
 
+    @Autowired
+    private FilterHtmlTagProcess testProcess;
 
     /**
      * Let's configure the Camel routing rules using Java code...
@@ -18,6 +24,7 @@ public class TimerToImapMailRouteBuilder extends RouteBuilder {
 
         from("pop3://localhost?username=mars@mars.bb.com&password=123456&debugMode=true&consumer.delay=60000")
                 .routePolicy(startPolicy)
+                .process(testProcess)
                 .log("mail body = ${body}")
                 .to("stream:out")
 //                .log("mail >> ${body}");
